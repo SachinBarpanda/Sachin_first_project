b00a29213a1fd649ddad6cbf3ebcda70ea6b92ff
Sachin <35636874+SachinBarpanda@users.noreply.github.com>
2018-06-20 15:15:16 +0530

    New Mobile navigation added

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -17,14 +17,137 @@ $(document).ready(function() {
    
     
     /*---------------------------------------*/
-    /*   For Sticky Navigation   */
+    /*   For    */
     /*---------------------------------------*/
 
     $('.js--scroll-to-plans').click(function () {
         $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
         
     });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+        
+    });
+    
+    /*---------------------------------------*/
+    /*  Navigation scroller   */
+    /*---------------------------------------*/
+    
+    
+    
+    
+    
+   $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
   
+    
+    /*---------------------------------------*/
+    /*  ANIMATIONS ON SCROLL   */
+    /*---------------------------------------*/
+   
+    $('.js--wp-1').waypoint(function(direction){
+                $('.js--wp-1').addClass('animated zoomIn');
+                        
+    },{
+        offset:'50%;'
+    });
+   
+   $('.js--wp-2').waypoint(function(direction){
+                $('.js--wp-2').addClass('animated fadeInUp');
+                        
+    },{
+        offset:'50%;'
+    });
+   
+    $('.js--wp-3').waypoint(function(direction){
+                $('.js--wp-3').addClass('animated fadeIn');
+                        
+    },{
+        offset:'70%;'
+    });
+   
+    $('.js--wp-4').waypoint(function(direction){
+                $('.js--wp-4').addClass('animated tada');
+                        
+    },{
+        offset:'30%;'
+    });
+   
+  
+    
+     /*---------------------------------------*/
+    /*  NAVIGATION MAKER FOR MOBILES   */
+    /*---------------------------------------*/
+    
+    $('.js--nav-icon').click(function(){
+        var nav =$('.js--main-nav');
+        var icon =$('.js--nav-icon i');
+        nav.slideToggle(400);
+        
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }
+        else{
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+            
+        }
+    })
     
     
 });
+
+
+
+//ion-close-round
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
